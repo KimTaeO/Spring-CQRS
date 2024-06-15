43da0b15f0ed8d3b1c1a7026559d05b6e7c447da
KimTaeO <taeo0328@gmail.com>
2024-06-15 18:08:47 +0900

    :fire: Lombok annotation

--- src/main/java/com/example/demo/domain/Board.java
+++ src/main/java/com/example/demo/domain/Board.java
@@ -1,12 +1,8 @@
 package com.example.demo.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
 @Builder
-@AllArgsConstructor
 public record Board(
         Integer id,
         String title,
