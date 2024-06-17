c2604595b8e4114f905cc561faad6ce3480f8c9c
KimTaeO <taeo0328@gmail.com>
2024-06-17 12:20:48 +0900

    :sparkles: Query all board web adapter

--- src/main/java/com/example/demo/command/presentation/BoardWebAdapter.java
+++ src/main/java/com/example/demo/command/presentation/BoardWebAdapter.java
@@ -1,22 +1,33 @@
 package com.example.demo.command.presentation;
 
 import com.example.demo.command.data.dto.request.SaveBoardRequest;
+import com.example.demo.command.data.dto.response.QueryAllBoardResponse;
+import com.example.demo.command.usecase.QueryAllBoardUseCase;
 import com.example.demo.command.usecase.SaveBoardUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class BoardWebAdapter {
     private final SaveBoardUseCase saveBoardUseCase;
+    private final QueryAllBoardUseCase queryAllBoardUseCase;
 
     @PostMapping
     public ResponseEntity<Void> saveBoard(@RequestBody SaveBoardRequest saveBoardRequest) {
         saveBoardUseCase.execute(saveBoardRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @GetMapping
+    public ResponseEntity<List<QueryAllBoardResponse>> queryAllBoard() {
+        List<QueryAllBoardResponse> response = queryAllBoardUseCase.QueryAllBoard();
+        return ResponseEntity.ok(response);
     }
 }
